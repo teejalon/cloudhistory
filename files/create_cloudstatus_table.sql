3c1cb7fbfd711955c6c1abcaa79e258a94bd861e
teejalon <teemu.jalonen@iki.fi>
2013-09-03 16:36:40 +0300

    modified:   create_cloudstatus_table.sql deleted unused fields

--- files/create_cloudstatus_table.sql
+++ files/create_cloudstatus_table.sql
@@ -2,8 +2,5 @@ CREATE TABLE "cloudstatushistory"
 (
 sampledatetime timestamp without time zone,
 testoutput text,
-productversion varchar(255),
-resourceoutout text,
-addressesout text,
 CONSTRAINT "cloudstatushistory_pkey" PRIMARY KEY (sampledatetime )
 );
