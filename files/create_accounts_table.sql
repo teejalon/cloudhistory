78defad28bb60738b98638549ff66587ba245205
teejalon <teemu.jalonen@iki.fi>
2013-10-22 13:34:25 +0300

    modified:   create_accounts_table.sql aded column for account email

--- files/create_accounts_table.sql
+++ files/create_accounts_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE "accounthistory"
 sampledatetime timestamp without time zone,
 AccountId varchar(255) NOT NULL,
 AccountName varchar(255),
+AccountEmail varchar(255),
 CONSTRAINT "accounthistory_pkey" PRIMARY KEY (AccountId )
 );
