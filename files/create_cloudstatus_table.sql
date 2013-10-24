fd023c0e8e33ff81378e2943f88e520da5ba519f
teejalon <teemu.jalonen@iki.fi>
2013-10-24 17:37:36 +0300

    modified:   cloudhistory-ec2.yml added move db to bacup server script and its cronjobs 	modified:   files/create_cloudstatus_table.sql added describeazverbose and zone fields

--- files/create_cloudstatus_table.sql
+++ files/create_cloudstatus_table.sql
@@ -1,6 +1,8 @@
 CREATE TABLE "cloudstatushistory"
 (
 sampledatetime timestamp without time zone,
+zone varchar(256),
 testoutput text,
+describeazverbose text,
 CONSTRAINT "cloudstatushistory_pkey" PRIMARY KEY (sampledatetime )
 );
