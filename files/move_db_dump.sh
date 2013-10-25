9ba7b0e5e71285cf99def121fa632d4a1fa1873d
teejalon <teemu.jalonen@iki.fi>
2013-10-25 14:21:23 +0300

    modified:   cloudhistory-ec2.yml changed cronjob to postgres user which has role in db already 	modified:   files/move_db_dump.sh  typos

--- files/move_db_dump.sh
+++ files/move_db_dump.sh
@@ -7,7 +7,7 @@ then
 else
 CLOUDNAME=$1
 BACKUP_SERVER={{ backupserver }}
-gzip -9 -f /tmp/cloudhistory$CLOUDNAME\.sql
-scp /tmp/cloudhistory$CLOUDNAME\.sql.gz $BACKUP_SERVER\:/opt/cloudbackup/$CLOUDNAME
-rm -f /tmp/cloudhistory$CLOUDNAME\.sql.gz
+gzip -9 -f /tmp/$CLOUDNAME\.sql
+scp /tmp/$CLOUDNAME\.sql.gz $BACKUP_SERVER\:/opt/cloudbackup/$CLOUDNAME
+rm -f /tmp/$CLOUDNAME\.sql.gz
 fi
